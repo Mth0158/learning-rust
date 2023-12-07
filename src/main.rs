e52ae751a7e54b72efcb413ff076ce5d80f3fc9a
Mth0158 <mathieu.eustachy@gmail.com>
2023-12-07 17:07:55 +0100

    [Tutorial] Use data types and math

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn main() {
 
     // tutorial_variables();
     // tutorial_constants();
+    // tutorial_data_types();
+    // tutorial_math();
 }
 
 fn tutorial_variables() {
@@ -31,4 +33,33 @@ fn tutorial_constants() {
         .expect("Age wasn't assigned a number");
     age = age + 1;
     println!("I'm {} and I want ${}", age, ONE_MIL);
+}
+
+fn tutorial_data_types() {
+    // Unsigned integer: u8, u16, u32, u64, u128, usize
+    // Signed integer: i8, i16, i32, i64, i128, isize
+    println!("Max u64: {}", u64::MAX);
+    println!("Max f64: {}", f64::MAX);
+    println!("Max i64: {}", i64::MAX);
+
+    let is_true = true;
+    let my_grade = 'A';
+}
+
+fn tutorial_math() {
+    let num_1: f32 = 1.111111111111111;
+    println!("f32: {}", num_1 + 0.111111111111111);
+    let num_2: f64 = 1.111111111111111;
+    println!("f64: {}", num_2 + 0.111111111111111);
+
+    let num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
+
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Random: {}", random_num);
 }
