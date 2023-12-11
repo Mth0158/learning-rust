e31ca347a9cd5627b764fffa06edabaebd088b97
Mth0158 <mathieu.eustachy@gmail.com>
2023-12-11 16:07:08 +0100

    [Tutorial] Use vectors

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ fn main() {
     // tutorial_tuples();
     // tutorial_strings();
     // tutorial_casting();
-    tutorial_enums();
+    // tutorial_enums();
+    tutorial_vectors();
 }
 
 fn tutorial_variables() {
@@ -219,4 +220,26 @@ fn tutorial_enums() {
     }
 
     println!("Is today the weekend? {}", today.is_weekend());
+}
+
+fn tutorial_vectors() {
+    // Are like arrays
+    let vec1: Vec<i32> = Vec::new();
+    let mut vec2 = vec![1,2,3,4];
+    vec2.push(5);
+    println!("1st: {}", vec2[0]);
+    let second: &i32 = &vec2[1];
+
+    match vec2.get(1) {
+        Some(second) => println!("2nd: {}", second),
+        None => println!("No 2nd value")
+    }
+    for i in &mut vec2 {
+        *i *= 2;
+    }
+    for i in &vec2 {
+        println!("{}", i);
+    }
+    println!("Vector length: {}", vec2.len());
+    println!("Pop: {:?}", vec2.pop());
 }
