839af6934e74d8c52db6c17f9d1de0facb7d8743
Mth0158 <mathieu.eustachy@gmail.com>
2023-12-15 19:57:42 +0100

    [Tutorial] Use reference smart pointers

--- src/main.rs
+++ src/main.rs
@@ -306,6 +306,12 @@ use std::collections::HashMap;
 mod restaurant;
 use crate::restaurant::order_food;
 
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
+
 fn main() {
     // play_with_ownership();
     // play_with_hasmaps();
@@ -315,7 +321,8 @@ fn main() {
     // play_with_errors_and_files();
     // play_with_iterators();
     // play_with_closures();
-    play_with_smart_pointers();
+    // play_with_smart_pointers();
+    play_with_concurrency();
 }
 
 fn play_with_ownership() {
@@ -510,4 +517,65 @@ fn play_with_smart_pointers() {
     let node1 = TreeNode::new(1)
         .left(TreeNode::new(2))
         .right(TreeNode::new(3));
+}
+
+fn play_with_concurrency() {
+    // let thread1 = thread::spawn(|| {
+    //     for i in 1..25 {
+    //         println!("Spawned thread: {}", i);
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+
+    // for i in 1..20 {
+    //     println!("Main thread: {}", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // thread1.join().unwrap();
+
+    pub struct Bank {
+        balance: f32,
+    }
+    // fn withdraw(the_bank: &mut Bank, amt: f32) {
+    //     the_bank.balance -= amt;
+    // }
+    // let mut bank = Bank{balance: 100.0};
+    // withdraw(&mut bank, 5.00);
+    // println!("Balance: {}", bank.balance);
+
+    // fn customer(the_bank: &mut Bank) {
+    //     withdraw(the_bank, 5.00)
+    // }
+
+    // thread::spawn(|| {
+    //     customer(&mut bank)
+    // }).join().unwrap();
+    fn withdraw(the_bank: &Arc<Mutex<Bank>>, amt: f32) {
+        let mut bank_ref = the_bank.lock().unwrap();
+        if bank_ref.balance < 5.00 {
+            println!("Current balance is: {}, Withdrawal a smaller amount", bank_ref.balance);
+        } else {
+            bank_ref.balance -= amt;
+            println!("Customer withdrew {}, Current balance {}", amt, bank_ref.balance);
+        }
+    }
+    fn customer(the_bank: Arc<Mutex<Bank>>) {
+        withdraw(&the_bank, 5.00);
+    }
+
+    let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(Bank {balance: 20.00}));
+    let handles = (0..10).map(|_| {
+        let bank_ref = bank.clone();
+
+        thread::spawn(|| {
+            customer(bank_ref);
+        })
+    });
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Total: {}", bank.lock().unwrap().balance);
+
 }
